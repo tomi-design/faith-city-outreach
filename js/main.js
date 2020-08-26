64bbd1069d99d7a368f9946db7c094ea09c5594f
Tomomi <tomoiwamoto21@gmail.com>
2020-08-26 00:55:40 -0400

    made paypal button larger

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 
 
-
 /*---For dropdown menu----*/
 
 function myFunction() {
@@ -26,79 +25,7 @@ window.onclick = function(event){
 
 
 
-/*------for slide show on homepage-----*/
-
-
-/*let slideIndex=0;
-showSlides();
-
-function showSlides(){
-    let i;
-    let slides= document.getElementsByClassName('slideshow');
-    for (i=0; i < slides.length; i++){
-        slides[i].style.display="none";
-    }
-
-    slideIndex++;
-    if (slideIndex > slides.length){
-        slideIndex=1
-    }
-
-    slides[slideIndex-1].style.display="block";
-    setTimeout(showSlides, 5000); 
-}*/
-
-
-
-
-/*---ORIGINAL var slideIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  
-  setTimeout(carousel, 3500); // Change image every 2 seconds
-}*/
-
-
-/*---third slideshow with jquery $("#slideshow > div:gt(0)").hide();
-
-setInterval(function() { 
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-},  3000);*/
-
-
-
-
-
-
-/*last version 
-
-var current = 0,
-    slides = document.getElementsByClassName("mySlides");
-
-setInterval(function() {
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.opacity = 0;
-  }
-  current = (current != slides.length - 1) ? current + 1 : 0;
-  slides[current].style.opacity = 1;
-}, 3000);
-*/
-
-
+/*------------sildeshow--------------*/
 
 
 const slideshowImages = document.querySelectorAll(".hero .mySlides");
@@ -120,4 +47,6 @@ function nextImage(){
     slideshowImages[currentImageCounter].style.opacity = 1;
     slideshowImages[currentImageCounter].style.zIndex = -1;
 }
+
+
 
