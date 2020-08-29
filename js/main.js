307cf6b155f69453a0d8dbe327fe15d14a47a9dc
Tomomi <tomoiwamoto21@gmail.com>
2020-08-29 00:58:26 -0400

    changed slideshow so that it doesn't require position properties

--- js/main.js
+++ js/main.js
@@ -22,27 +22,48 @@ window.onclick = function(event){
 
 
 
-/*------------sildeshow--------------*/
+// /*------------sildeshow--------------*/
 
 
-const slideshowImages = document.querySelectorAll(".hero .mySlides");
+// const slideshowImages = document.querySelectorAll(".hero .mySlides");
 
-const nextImageDelay = 5000; 
-let currentImageCounter= 0;
+// const nextImageDelay = 5000; 
+// let currentImageCounter= 0;
 
-slideshowImages[currentImageCounter].style.opacity = 1;
-setInterval(nextImage, nextImageDelay);
+// slideshowImages[currentImageCounter].style.opacity = 1;
+// setInterval(nextImage, nextImageDelay);
 
-function nextImage(){
-    //slideshowImages[currentImageCounter].style.opacity = 0;
-    slideshowImages[currentImageCounter].style.zIndex = -2;
-    const tempCounter = currentImageCounter;
-    setTimeout(() => {
-        slideshowImages[tempCounter].style.opacity = 0;
-    }, 1000);
-    currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
-    slideshowImages[currentImageCounter].style.opacity = 1;
-    slideshowImages[currentImageCounter].style.zIndex = -1;
+// function nextImage(){
+//     //slideshowImages[currentImageCounter].style.opacity = 0;
+//     slideshowImages[currentImageCounter].style.zIndex = -2;
+//     const tempCounter = currentImageCounter;
+//     setTimeout(() => {
+//         slideshowImages[tempCounter].style.opacity = 0;
+//     }, 1000);
+//     currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
+//     slideshowImages[currentImageCounter].style.opacity = 1;
+//     slideshowImages[currentImageCounter].style.zIndex = -1;
+// }
+
+
+/*----test slideshow----*/
+
+let slideIndex=0;
+showSlides();
+
+function showSlides(){
+    let i;
+    let slides= document.getElementsByClassName('slideshow');
+    for (i=0; i < slides.length; i++){
+        slides[i].style.display="none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length){
+        slideIndex=1
+    }
+
+    slides[slideIndex-1].style.display="block";
+    setTimeout(showSlides, 5000); 
 }
-
 
