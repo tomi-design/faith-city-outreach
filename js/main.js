5ab4b74cf7a39e61e65d6a454f99100f4914edf5
Tomomi <58757939+tomi-design@users.noreply.github.com>
2020-09-17 23:38:25 -0400

    Changed slideshow to Slickslideshow. Added slick folder to the root

--- js/main.js
+++ js/main.js
@@ -48,22 +48,23 @@ window.onclick = function(event){
 
 /*----new slideshow----*/
 
-let slideIndex=0;
-showSlides();
+// let slideIndex=0;
+// showSlides();
 
-function showSlides(){
-    let i;
-    let slides= document.getElementsByClassName('slideshow');
-    for (i=0; i < slides.length; i++){
-        slides[i].style.display="none";
-    }
+// function showSlides(){
+//     let i;
+//     let slides= document.getElementsByClassName('slideshow');
+//     for (i=0; i < slides.length; i++){
+//         slides[i].style.display="none";
+//     }
 
-    slideIndex++;
-    if (slideIndex > slides.length){
-        slideIndex=1
-    }
+//     slideIndex++;
+//     if (slideIndex > slides.length){
+//         slideIndex=1
+//     }
 
-    slides[slideIndex-1].style.display="block";
-    setTimeout(showSlides, 5000); 
-}
+//     slides[slideIndex-1].style.display="block";
+//     setTimeout(showSlides, 5000); 
+// }
 
+/*----------TEST SLIDESHOW-------------*/
